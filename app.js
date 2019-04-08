26476d4527d8c7a2dbe3ce1d546b557c65ff1268
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-08 13:58:56 -0500

    events

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ new Vue({
   data: {
     greeting: 'Hello World!',
     link : 'http://www.google.com',
-    finishedLink: '<a href="http://www.google.com">Google</a>'
+    finishedLink: '<a href="http://www.google.com">Google</a>',
+    counter: 0,
+    x : 0,
+    y:0
   },
   methods: {
     changeGreeting: function (event) {
@@ -11,6 +14,16 @@ new Vue({
     },
     sayHello(){
       return this.greeting;
+    },
+    increase : function(step, event){
+      this.counter += step;
+    },
+    updateCoordinates: function(event){
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe : function(){
+      alert('Alert!!!')
     }
   }
 })
