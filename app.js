c0aba5bda9dcc95ff180a42537ae002535d2e7af
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-08 11:59:45 -0500

    use v-bind and v-html directives

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 new Vue({
-    el: '#app',
-    data: {
-        greeting: 'Hello World!'
+  el: '#app',
+  data: {
+    greeting: 'Hello World!',
+    link : 'http://www.google.com',
+    finishedLink: '<a href="http://www.google.com">Google</a>'
+  },
+  methods: {
+    changeGreeting: function (event) {
+      this.greeting = event.target.value;
     },
-    methods: {
-        changeGreeting: function(event) {
-            this.greeting = event.target.value;
-        }
+    sayHello(){
+      return this.greeting;
     }
+  }
 })
