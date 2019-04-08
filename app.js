834f84a059157aca7590069d1af23dd920b7f7c1
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-08 14:17:47 -0500

    two way binding

--- app.js
+++ app.js
@@ -2,27 +2,28 @@ new Vue({
   el: '#app',
   data: {
     greeting: 'Hello World!',
-    link : 'http://www.google.com',
+    link: 'http://www.google.com',
     finishedLink: '<a href="http://www.google.com">Google</a>',
     counter: 0,
-    x : 0,
-    y:0
+    x: 0,
+    y: 0
+    , name: 'Julian'
   },
   methods: {
     changeGreeting: function (event) {
       this.greeting = event.target.value;
     },
-    sayHello(){
+    sayHello() {
       return this.greeting;
     },
-    increase : function(step, event){
+    increase: function (step, event) {
       this.counter += step;
     },
-    updateCoordinates: function(event){
+    updateCoordinates: function (event) {
       this.x = event.clientX;
       this.y = event.clientY;
     },
-    alertMe : function(){
+    alertMe: function () {
       alert('Alert!!!')
     }
   }
