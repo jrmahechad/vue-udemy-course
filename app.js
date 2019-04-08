1eba33ca51363e3c56a00550c40b00cdfc679319
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-08 14:03:13 -0500

    start assigment

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 new Vue({
-  el: '#app',
+  el: '#exercise',
   data: {
-    greeting: 'Hello World!',
-    link : 'http://www.google.com',
-    finishedLink: '<a href="http://www.google.com">Google</a>',
-    counter: 0,
-    x : 0,
-    y:0
-  },
-  methods: {
-    changeGreeting: function (event) {
-      this.greeting = event.target.value;
-    },
-    sayHello(){
-      return this.greeting;
-    },
-    increase : function(step, event){
-      this.counter += step;
-    },
-    updateCoordinates: function(event){
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    alertMe : function(){
-      alert('Alert!!!')
-    }
+      value: ''
   }
-})
+});
