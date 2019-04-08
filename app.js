567e3ad0084ac451da094803fc162f6a9196d995
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-08 14:12:24 -0500

    solution

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ new Vue({
   el: '#exercise',
   data: {
       value: ''
+  },
+  methods: {
+    showAlert: function(){
+      alert("Alert!!!!");
+    },
+    storeValue: function(event){
+      this.value = event.target.value
+    }
   }
 });
