964c96275ad9460aff2c6abdea39c49113ff459b
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-09 09:10:48 -0500

    solution

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ new Vue({
   el: '#app',
   data: {
     value: 0
+  },
+  computed: {
+    result: function(){
+      return this.value < 37 ? 'not there yet' : 'Done!'
+    }
+  },
+  watch: {
+    result: function(value){
+      var vm = this;
+      setTimeout(() => {
+        vm.value = 0;
+      }, 5000)
+    }
   }
 })
