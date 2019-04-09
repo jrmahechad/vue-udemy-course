8af81be957e490353322c992eb77c1e301359ac6
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-09 10:36:38 -0500

    solution

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 new Vue({
   el: '#app',
   data: {
-
+    myEffect:'',
+    effects: ['highlight','shrink'],
+    class1: 'blue',
+    class2: 'height200',
+    classFromInput:'blue',
+    classToggle: true,
+    styleFromInput:'',
+    progressN : 0
   },
   methods: {
     startEffect: function() {
-    
+      var vm = this;
+      setInterval(() => {
+        vm.myEffect = vm.myEffect === vm.effects[1] ? vm.effects[0]: vm.effects[1];
+        
+      }, 2000);
+    },
+    startProgressBar: function(){
+      var vm = this;
+      setInterval(() => {
+        vm.progressN +=10;
+        vm.progressN = vm.progressN <= 500 ? vm.progressN : 0
+      }, 2000);
     }
   }
 })
