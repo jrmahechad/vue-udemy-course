52e74363681fb754e2de57a628d2dc26330d8496
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-04-10 11:05:46 -0500

    start

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 new Vue({
   el: '#app',
   data: {
-    show:true,
-    ingredients: ['meat', 'fruit', 'cookies'],
-    persons: [
-      {name: 'Max', age: '27', color: 'red'},
-      {name: 'Anna', age: '29', color: 'blue'}
-    ]
-    
-  },
-})
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
