b4618a6ea090407b5ce328a14288176d2271bd3e
jrmahechad <jmahecha@belatrixsf.com>
2019-04-19 16:30:16 -0500

    working with instances

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-new Vue({
-  el: '#app1',
-  data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
-  },
+var data = {
+  title: "The VueJS Instance",
+  showParagraph: false
+};
+
+Vue.component("hello", {
+  template: "<h1>Hello from component!!!</h1>"
+});
+
+var vm1 = new Vue({
+  data: data,
   methods: {
     show: function() {
       this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
+      this.updateTitle("The VueJS Instance (Updated)");
+      this.$refs.myButton.innerText = "Text";
+      console.log(this.$refs);
     },
     updateTitle: function(title) {
       this.title = title;
@@ -20,14 +27,74 @@ new Vue({
   },
   watch: {
     title: function(value) {
-      alert('Title changed, new value: ' + value);
+      alert("Title changed, new value: " + value);
     }
   }
 });
 
-new Vue({
-  el:'#app2',
-  data:{
-    title:'The second instance'
+setTimeout(() => {
+  vm1.title = "Changed by timer!!";
+  vm1.show();
+}, 3000);
+
+var vm2 = new Vue({
+  el: "#app2",
+  data: {
+    title: "The second instance"
+  },
+  methods: {
+    onChange: function() {
+      vm1.title = "Changed!!";
+    }
   }
-})
+});
+
+vm1.$mount("#app1");
+vm1.$refs.heading.innerText = "Something else";
+console.log(vm1);
+console.log(vm1.$data === data);
+
+var vm3 = new Vue({
+  template: "<h1>Hello!!</h1>"
+});
+
+//vm3.$mount('#app3') Mount the vue instance passing the selector
+// Or create the view instance and the attach it using JS
+vm3.$mount("");
+document.getElementById("app3").appendChild(vm3.$el);
+
+var vm4 = new Vue({
+  el: "#app4",
+  data:{
+    title: 'TITLE'
+  },
+  methods:{
+    destroy: function(){
+      this.$destroy();
+    }
+  },
+  beforeCreate: function() {
+    console.log("beforeCreate");
+  },
+  created: function() {
+    console.log("created");
+  },
+  beforeMount: function() {
+    console.log("beforeMount");
+  },
+  mounted: function() {
+    console.log("mounted");
+  },
+  beforeUpdate: function() {
+    console.log("beforeUpdate");
+  },
+  updated: function() {
+    console.log("updated");
+  },
+  beforeDestroy: function() {
+    console.log("beforeDestroy");
+  },
+  destroyed: function() {
+    console.log("destroyed");
+  }
+});
