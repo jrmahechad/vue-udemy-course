b98d24243fb49858dfa98130448461ca27819c0c
jrmahechad <jmahecha@belatrixsf.com>
2019-04-28 17:58:34 -0500

    solution

---
+++ src/Servers.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+        <app-server-details v-for="item in 5" ></app-server-details>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
+<script>
+import ServerDetails from './ServerDetails.vue';
+export default {
+  components: {
+      'app-server-details': ServerDetails
+  }
+};
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+
+Vue.component('app-header', Header);
+Vue.component('app-footer', Footer);
+
 
 new Vue({
   el: '#app',
