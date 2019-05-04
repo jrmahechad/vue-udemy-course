d22a6cdb0afaaec4d6976144880873095bff38b5
jrmahechad <jmahecha@belatrixsf.com>
2019-05-04 15:27:11 -0500

    solution

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,13 +1,41 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+  <div class="col-xs-12 col-sm-6">
+    <p>Server Details are currently not updated</p>
+    <div v-if="this.id>0">
+      <p>Server # {{this.id}}</p>
+      <p>Server Status {{this.status}}</p>
+      <button @click="changeStatusNormal">Change Status to Normal</button>
     </div>
-
+  </div>
 </template>
 
 <script>
+import { eventBus } from "../../main";
+export default {
+  data: function() {
+    return {
+      id: 0,
+      status: ""
+    };
+  },
+  methods: {
+    changeStatusNormal: function() {  
+      const server = {
+          id: this.id,
+          status: 'Normal'
+      }
+      this.id = 0;
+      eventBus.updateServerDetails(server);
+    }
+  },
+  created() {
+    eventBus.$on("openServerDetails", server => {
+      this.id = server.id;
+      this.status = server.status;
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
