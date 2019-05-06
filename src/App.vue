f451714487f4482cfa7e6c022026bb756967132a
jrmahechad <jmahecha@belatrixsf.com>
2019-05-05 19:31:58 -0500

    progress video 143

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <app-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!!</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header";
+export default {
+  data: function() {
+    return {
+      maxQuotes: 10,
+      quotes: ["Just a simgle Quote"]
+    };
+  },
+  methods: {
+    newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes){
+            return alert('Please delete quotes first!!')
+        }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
