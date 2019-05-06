895d5dcf51b3af796b5e8fa928510839b596c9a2
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-05-06 09:37:27 -0500

    finish

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,15 +1,48 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>Name: {{switchName()}}</p>
+    <p>Age: {{userAge}}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn">Reset Name from parent Callback</button>
+  </div>
 </template>
 
 <script>
-</script>
-
-<style scoped>
-    div {
-        background-color: lightcoral;
+import { eventBus } from "../main";
+export default {
+  props: {
+    myName: {
+      type: String,
+      required: true,
+      default: "Default"
+    },
+    userAge: Number,
+    resetFn: Function
+  },
+  methods: {
+    switchName() {
+      return this.myName
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      this.myName = "Julian";
+      this.$emit("nameWasReset", this.myName);
     }
+  },
+  created() {
+    eventBus.$on('ageWasEdited', (age) => {
+        this.userAge = age;
+    });
+  }
+};
+</script>
+       
+<style scoped>
+div {
+  background-color: lightcoral;
+}
 </style>
