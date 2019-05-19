f36a03d00f17ccbd49e6f3b0be524ebbe2432f07
jrmahechad <jmahecha@belatrixsf.com>
2019-05-19 08:53:29 -0500

    solution

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,47 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Directives</h1>
-        <p v-text="'Some text'"></p>
-        <p v-html="'<strong>Some text in bold</strong>'"></p>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
+                <hr>
+                <div
+                        style="width: 100px; height: 100px; background-color: lightgreen"
+                        v-customOn:mouseenter="mouseEnter"
+                        v-customOn:mouseleave="mouseLeave"></div>
+            </div>
+        </div>
     </div>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-highlight:background="'red'" v-highlight="'white'">Color this</p>
-        <p v-highlight="'red'">Color this</p>
-        <p
-          v-local-highlight:background.delayed.blink="{mainColor: 'green', secondColor:'blue', delay:500}"
-        >Color this</p>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-export default {
-  directives: {
-    "local-highlight": {
-      bind(el, binding, vnode) {
-        let delay = 0;
-        if (binding.modifiers["delayed"]) {
-          delay = 3000;
-        }
-        if (binding.modifiers["blink"]) {
-          let mainColor = binding.value.mainColor;
-          let secondColor = binding.value.secondColor;
-          let currentColor = mainColor;
-          setTimeout(() => {
-            setInterval(() => {
-              currentColor =
-                currentColor === secondColor ? mainColor : secondColor;
-              if (binding.arg === "background") {
-                el.style.backgroundColor = currentColor;
-              } else {
-                el.style.color = currentColor;
-              }
-            }, binding.value.delay);
-          }, delay);
-        } else {
-          setTimeout(() => {
-            if (binding.arg === "background") {
-              el.style.backgroundColor = binding.value.mainColor;
-            } else {
-              el.style.color = binding.value.mainColor;
+    export default {
+        directives: {
+            customOn: {
+                bind(el, binding) {
+//                    el.onclick = function() {
+//                        binding.value();
+//                    }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
             }
-          }, delay);
+        },
+        methods: {
+            clicked() {
+                alert('I was clicked!');
+            },
+            mouseEnter() {
+                console.log('Mouse entered!');
+            },
+            mouseLeave() {
+                console.log('Mouse leaved!');
+            }
         }
-      }
     }
-  }
-};
 </script>
 
 <style>
