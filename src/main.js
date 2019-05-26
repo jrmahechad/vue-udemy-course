7fda97b1433efd71623a2055785eac2e04b855d2
jrmahechad <jmahecha@belatrixsf.com>
2019-05-26 17:30:23 -0500

    finish

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResource);
+Vue.http.options. root = "https://vue-course-udemy.firebaseio.com/";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next( response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      }
+    }
+  });
+});
 
 new Vue({
   el: '#app',
