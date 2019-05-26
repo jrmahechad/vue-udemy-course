924e496daba9d41f8415b75a370be1a1c5cb4a7b
jrmahechad <jmahecha@belatrixsf.com>
2019-05-26 15:15:57 -0500

    start

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
-Vue.filter('append-length', function(value) {
-    return `${value} (${value.length})`;
-});
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
