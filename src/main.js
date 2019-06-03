bc4897bed44fd7f78fa1d427ab9e665aa639ba51
jrmahechad <jmahecha@belatrixsf.com>
2019-06-03 08:51:07 -0500

    start routing

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource);
-Vue.http.options. root = "https://vue-course-udemy.firebaseio.com/";
-Vue.http.interceptors.push((request, next) => {
-  console.log(request);
-  if(request.method == 'POST'){
-    request.method = 'PUT';
-  }
-  next( response => {
-    response.json = () => {
-      return {
-        messages: response.body
-      }
-    }
-  });
-});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-});
+})
