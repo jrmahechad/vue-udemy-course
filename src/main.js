ebd056486942221488abe457f1f2b237e4158ec4
jrmahechad <jmahecha@belatrixsf.com>
2019-06-03 10:38:57 -0500

    finish routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: "history", // To remove # from the url, server needs to be configured
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('global beforeEach');
+  next();
+  //next(false); abort operation
+  //next('/')
+  //next({path:'/'})
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
+  router,
   render: h => h(App)
-})
+});
