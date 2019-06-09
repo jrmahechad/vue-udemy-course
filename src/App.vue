62d5e9f15438beaa2b01206db4aa14b9b5f183cc
jrmahechad <jmahecha@belatrixsf.com>
2019-06-09 11:10:09 -0500

    start

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,30 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Routing</h1>
-        <hr>
-        <router-view name="header-top"></router-view>
-        <transition name="slide" mode="out-in">
-          <router-view></router-view>
-        </transition>
-        <router-view name="header-bottom"></router-view>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result :counter="counter"></app-result>
+                <hr>
+                <app-counter @updated="counter += $event"></app-counter>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-export default {
-  components: {
-    appHeader: Header
-  }
-};
+    import Counter from './components/Counter.vue';
+    import Result from './components/Result.vue';
+
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        components: {
+            appCounter: Counter,
+            appResult: Result,
+        }
+    }
 </script>
 
-<style>
-.slide-enter {
-  opacity: 0;
-}
-.slide-enter-active {
-  animation: slide-in 1s ease-out forwards;
-  transition: opacity 0.5s;
-}
-.slide-leave {
-}
-.slide-leave-active {
-  animation: slide-out 1s ease-out forwards;
-  transition: opacity 1s;
-  opacity: 0;
-  position: absolute;
-}
-.slide-move {
-  transition: transform 1s;
-}
-@keyframes slide-in {
-  from {
-    transform: translateY(20px);
-  }
-  to {
-    transform: translateY(0);
-  }
-}
-@keyframes slide-out {
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(20px);
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,34 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueRouter from "vue-router";
-import { routes } from "./routes";
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  routes,
-  mode: "history", // To remove # from the url, server needs to be configured
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash) {
-      return { selector: to.hash };
-    }
-    return { x: 0, y: 0 };
-  }
-});
-
-router.beforeEach((to, from, next) => {
-  console.log('global beforeEach');
-  next();
-  //next(false); abort operation
-  //next('/')
-  //next({path:'/'})
-});
+import Vue from 'vue'
+import App from './App.vue'
 
 new Vue({
-  el: "#app",
-  router,
+  el: '#app',
   render: h => h(App)
-});
+})
