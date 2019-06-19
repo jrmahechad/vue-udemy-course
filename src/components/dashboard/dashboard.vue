2d6f4d1ae29fd49384dfa28c7fae5715189ccb3f
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-06-19 09:32:43 -0500

    axios

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,8 +2,41 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Youe email address: {{email}}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return{
+      email: ''
+    }
+  },
+  created() {
+    axios.get('/users.json')
+    .then((response)=>{
+      console.log(response);
+      const data = response.data;
+      const users = [];
+      for(let key in data) {
+        const user = data[key];
+        user.id = key;
+        users.push(user);
+      }
+      this.email = users[0].email;
+      console.log(users);
+      
+      
+    })
+    .catch((error)=>{
+      console.log(error);
+      
+    });
+  }
+}
+</script>
 
 <style scoped>
   h1, p {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL = 'https://vue-course-axios-aefda.firebaseio.com';
+axios.defaults.headers.common['Authorization'] = 'asdasd';
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+const reqInterceptor = axios.interceptors.request.use((config)=>{
+  console.log('Request Interceptor',config);
+  return config;
+})
+
+const resInterceptor = axios.interceptors.response.use((res)=>{
+  console.log('Response Interceptor',res);
+  return res;
+})
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
 
 new Vue({
   el: '#app',
