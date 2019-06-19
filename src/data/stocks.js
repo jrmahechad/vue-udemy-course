718cce55d5fe2b88a4e9e7462069e0ec1b784ee0
Julian Mahecha <mahecha@mahecha-macbookpro.roam.corp.google.com>
2019-06-19 08:48:52 -0500

    project setup

--- src/data/stocks.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { id: 1, name: 'BMW', price: 110 },
-    { id: 2, name: 'Google', price: 120 },
-    { id: 3, name: 'Apple', price: 130 },
-    { id: 4, name: 'Twitter', price: 140 },
-]
--- src/main.js
+++ src/main.js
@@ -1,23 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
 import App from './App.vue'
-import { routes } from "./routes";
-import store from "./store/store";
 
-Vue.use(VueRouter);
-Vue.use(VueResource);
-
-Vue.http.options.root= 'https://vue-course-stock-trader-806be.firebaseio.com/';
-
-Vue.filter('currency', (value)=>{
-  return '$' + value.toLocaleString();
-})
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
-});
+import router from './router'
+import store from './store'
 
 new Vue({
   el: '#app',
